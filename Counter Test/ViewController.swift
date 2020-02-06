94c147bac75ebda545e3d352c55c4f5a6d46e6b6
P. Wahl <philip.jacob.wahl@gmail.com>
2020-02-05 20:56:43 -0800

    Initial Commit

---
+++ Counter Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter Test
+//
+//  Created by Philip Wahl on 2/5/20.
+//  Copyright © 2020 Practice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
