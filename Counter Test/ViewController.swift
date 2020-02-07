a92b9f4f0b96bba6dfd570de4a4595bb8ddac4f1
P. Wahl <philip.jacob.wahl@gmail.com>
2020-02-06 22:05:45 -0800

    counter

--- Counter Test/ViewController.swift
+++ Counter Test/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
 
+    @IBOutlet weak var myFirstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myFirstLabel.text = "0"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        count += 1
+        myFirstLabel.text = String(count)
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        count = 0
+        myFirstLabel.text = String(count)
+    }
 }
 
